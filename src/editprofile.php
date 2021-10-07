33c2257c93dfc94280406c79a543a73a1f8544e7
Ashin Berish <asinberish@gmail.com>
2021-10-07 11:14:45 +0530

    Fixed CSS path in HTML

--- src/editprofile.php
+++ src/editprofile.php
@@ -23,7 +23,7 @@ else{
 			integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
 			crossorigin="anonymous"
 		/>
-		<link rel="stylesheet" href="styles/dashboard.css" />
+		<link rel="stylesheet" href="../styles/dashboard.css" />
 		<script
 			src="https://code.jquery.com/jquery-3.6.0.min.js"
 			integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
--- src/signup.php
+++ src/signup.php
@@ -11,7 +11,7 @@
 			integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
 			crossorigin="anonymous"
 		/>
-		<link rel="stylesheet" href="styles/signup.css" />
+		<link rel="stylesheet" href="../styles/signup.css" />
 		<script
 			src="https://code.jquery.com/jquery-3.6.0.min.js"
 			integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
