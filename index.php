dc6c9f0c366e1c3a57fe03e1f5164f8024b9be78
Ashin Berish <asinberish@gmail.com>
2021-10-07 11:06:22 +0530

    added composer

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 include "config.php";
 if(!isset($_SESSION['email'])){
-   header('Location: signin.php');
+   header('Location: ./src/signin.php');
 }
 else{
-    header('Location: dashboard.php');
+    header('Location: ./src/dashboard.php');
 }
--- src/dashboard.php
+++ src/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 if(!isset($_SESSION['email'])){
-   header('Location: index.php');
+   header('Location: ../index.php');
 }
 else{
     $email = $_SESSION['email'];
